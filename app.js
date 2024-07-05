babafbeaee8c9e8fb09e5a7da52991efedf7a13b
gusyaaa <askerzadehuseyn2005@gmail.com>
2024-07-05 18:09:13 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+let date = prompt("Enter the number of the month")
+switch(date){
+    case "1" :
+    case "01" :
+        console.log("January")
+    break;
+    case "2" :
+    case "02" :
+        console.log("February")
+    break;
+    case "3" :
+    case "03" :
+        console.log("March")
+    break;
+    case "4" :
+    case "04" :
+        console.log("April")
+    break;
+    case "5" :
+    case "05" :
+        console.log("May")
+    break;
+    case "6" :
+    case "06" :
+        console.log("June")
+    break;
+    case "7" :
+    case "07" :
+        console.log("July")
+    break;
+    case "8" :
+    case "08" :
+        console.log("August")
+    break;
+    case "9" :
+    case "09" :
+        console.log("September")
+    break;
+    case "10" :
+        console.log("October")
+    break;
+    case "11" :
+        console.log("November")
+    break;
+    case "12" :
+        console.log("December")
+    break;
+    default:
+        console.log("Your input is incorrect")
+}
